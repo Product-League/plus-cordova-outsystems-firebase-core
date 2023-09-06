fcee85078fff15324ba17bc764150601b0bd013c
psolipaPL <126564692+psolipaPL@users.noreply.github.com>
2023-09-06 13:48:09 +0100

    Update iosTest.js

--- hooks/iosTest.js
+++ hooks/iosTest.js
@@ -1,75 +1,29 @@
 "use strict";
 
 var path = require("path");
-var AdmZip = require("adm-zip");
 
 var utils = require("./utilities");
+let xml2js = require('xml2js');
 
 var constants = {
   googleServices: "google-services"
 };
 
 module.exports = function(context) {
-  var cordovaAbove8 = utils.isCordovaAbove(context, 8);
-  var cordovaAbove7 = utils.isCordovaAbove(context, 7);
-  var defer;
-  if (cordovaAbove8) {
-    defer = require("q").defer();
-  } else {
-    defer = context.requireCordovaModule("q").defer();
-  }
-  
-  var platform = context.opts.plugin.platform;
-  var platformConfig = utils.getPlatformConfigs(platform);
-  if (!platformConfig) {
-    utils.handleError("Invalid platform", defer);
-  }
-   
-  const iosConf = utils.getiOSConfigs();
 
-  var wwwPath = utils.getResourcesFolderPath(context, 'ios', iosConf);
-  var sourceFolderPath = utils.getSourceFolderPath(context, wwwPath);
-  var googleServicesZipFile = utils.getZipFile(sourceFolderPath, constants.googleServices);
-  if (!googleServicesZipFile) {
-    throw new Error("No configuration zip file found (google-services-zip).");
-  }
+  function getAppIdentifier(configPath) {
+    const parseString = xml2js.parseString;
+    const config_xml = fs.readFileSync(configPath).toString();
+    let appId;
 
-  var zip = new AdmZip(googleServicesZipFile);
+    parseString(config_xml, (err, config) => {
+        if (err) return console.error(err);
 
-  var targetPath = path.join(wwwPath, constants.googleServices);
-  zip.extractAllTo(targetPath, true);
+        console.log("App identifier: " + config['widget']['$'].id);
+        appId = config['widget']['$'].id;
+    })
+    return appId;
+}
 
-  var files = utils.getFilesFromPath(targetPath);
-  if (!files) {
-    utils.handleError("No directory found", defer);
-  }
-
-  var fileName = files.find(function (name) {
-    return name.endsWith(iosConf.firebaseFileExtension);
-  });
-  if (!fileName) {
-    //utils.handleError("No file found", defer);
-    throw new Error('Firebase plist file not found');
-  }
-
-  var sourceFilePath = path.join(targetPath, fileName);
-  console.log(sourceFilePath)
-  var destFilePath = path.join(context.opts.plugin.dir, fileName);
-  console.log(destFilePath)
-
-  if(!utils.checkIfFolderExists(destFilePath)){
-    utils.copyFromSourceToDestPath(defer, sourceFilePath, destFilePath);
-  }
-
-  if (cordovaAbove7) {
-    var destPath = path.join(context.opts.projectRoot, "platforms", "ios", "app");
-    if (utils.checkIfFolderExists(destPath)) {
-      var destFilePath = path.join(destPath, fileName);
-      if(!utils.checkIfFolderExists(destFilePath)){
-        utils.copyFromSourceToDestPath(defer, sourceFilePath, destFilePath);
-      }
-    }
-  }
-      
-  return defer.promise;
+  fs.copyFileSync(context.opts.projectRoot + '/platforms/ios/www/' + getAppIdentifier(context.opts.projectRoot + '/platforms/ios/ECOP Mobile/config.xml') + '.firebase' + '/GoogleService-Info.plist' , path.join(context.opts.plugin.dir, 'GoogleService-Info.plist'));
 }
